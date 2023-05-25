fc62f400db8efc316379ecd59ec2100d1881ad9a
EuryJesus <Eury0218@hotmail.com>
2023-05-25 15:23:58 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+
+const newHeader = document.createElement('h1')
+newHeader.textContent = 'victory'
+newHeader.textContent =' YOUR-NAME is the champion'
+newHeader.setAttribute('id','victory')
